3fc94ad429ac148a9f54e37f36a49df39172b80a
MasaoFujii <masao.fujii@gmail.com>
2015-08-26 23:10:34 +0900

    Improve regression test coverage of pgs2snippet1 function.

--- sql/ludia_funcs.sql
+++ sql/ludia_funcs.sql
@@ -1,11 +1,13 @@
+\pset null '(null)'
+
+-- Load ludia_funcs module
 LOAD '$libdir/ludia_funcs';
 CREATE EXTENSION ludia_funcs;
 
 SET standard_conforming_strings TO off;
 SET escape_string_warning TO off;
 
-\pset null '(null)'
-
+-- pgs2snippet1() checks
 SET ludia_funcs.escape_snippet_keyword TO on;
 
 SELECT pgs2snippet1(1, 300, 1, '@', '@', 0, '"\\"', '\\');
@@ -128,3 +130,18 @@ SELECT pgs2snippet1(1, 300, 1, '@', '@', 0, '"AA"A"   "BBB" - "CCC"', 'AA"ABBBCC
 SELECT pgs2snippet1(1, 300, 1, '@', '@', 0, '"AA"A" - "BBB" + "CCC"', 'AA"ABBBCCCDDDEEE');
 SELECT pgs2snippet1(1, 300, 1, '@', '@', 0, '"AA"A" - "BBB"   "CCC"', 'AA"ABBBCCCDDDEEE');
 SELECT pgs2snippet1(1, 300, 1, '@', '@', 0, '"AA"A" - "BBB" - "CCC"', 'AA"ABBBCCCDDDEEE');
+
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,NULL,'あｲうｴおａbｃdｅかｷくｹこjｋlｍn');
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'あｲうｴおａbｃdｅかｷくｹこjｋlｍn',NULL);
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'エおA','あｲうｴおａbｃdｅかｷくｹこjｋlｍn');
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'えおA','あｲうｴおａbｃdｅかｷくｹこjｋlｍn');
+
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'エおA',repeat(chr(13078),10) || chr(65018) || 'あｲうｴおａbｃdｅ' || chr(65018) || repeat(chr(13078),10) || 'かｷくｹこjｋlｍn' || chr(65018) || repeat(chr(13078),10));
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'""' || chr(13078) || chr(65018) || 'かキ""',chr(65018) || 'あｲうｴおａbｃdｅ' || chr(65018) || repeat(chr(13078),10) || 'かｷくｹこjｋlｍn' || chr(65018)||repeat(chr(13078),10));
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'"' || chr(13078) || chr(65018) || 'abｶ'|| '"','"' || repeat(chr(13078),10) || chr(65018) || 'あｲうｴおａbｃdｅ' || chr(65018) || repeat(chr(13078),10) || 'かｷくｹこjｋlｍn' || chr(65018) || repeat(chr(13078),10) || '"');
+SELECT pgs2snippet1(1,3000,1,'∇','∇',0,'"' || chr(13078) || chr(65018) || '"','"' || repeat(chr(13078),10)|| repeat(chr(65018),10) || '"');
+SELECT pgs2snippet1(1,3000,1,'∇','∇',0,'"' || repeat(chr(13078),10) || repeat(chr(65018),10) || '"','"' || chr(13078) || chr(65018)|| '"');
+
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,repeat('x',300),'abcde');
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,repeat('+',300),'+');
+SELECT pgs2snippet1(1,300,1,'∇','∇',0,'エおA','あｲうｴおａbｃdｅかｷくｹこjｋlｍn') FROM generate_series(1,10);
